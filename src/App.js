c8e6d21eec1f08ca0bbaceeeb477ce6373600124
zafer k <zaferkirca.tr@gmail.com>
2020-12-19 19:23:27 +0300

    used variable

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import News from "./components/News";
 
 function App() {
+    const name = 'Zafer KIRCA';
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
+
+                {name}
+
                 <News></News>
             </header>
         </div>
