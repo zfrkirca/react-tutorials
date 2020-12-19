6c1e061993032c5967a6ccb815956dc8ab65f63f
zafer k <zaferkirca.tr@gmail.com>
2020-12-19 18:23:24 +0300

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class NewsItem extends Component {
+    render() {
+        return (
+            <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit
+            </div>
+        )
+    }
+}
+
+export default NewsItem;
