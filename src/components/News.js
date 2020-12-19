82de6a97388b1b1fc6c05791fcbe9fa6d96f8008
zafer k <zaferkirca.tr@gmail.com>
2020-12-19 19:39:16 +0300

    used foreach

--- src/components/News.js
+++ src/components/News.js
@@ -5,23 +5,27 @@ class News extends Component {
     render() {
         const news = [
             {
+                id: 1,
                 title: "title1",
                 description: "description1"
             }, {
+                id: 2,
                 title: "title2",
                 description: "description2"
             }, {
+                id: 3,
                 title: "title3",
                 description: "description3"
-            }, {
-                title: "title4",
-                description: "description4"
-            },
+            }
         ];
 
         return (
             <div>
-                <NewsItem title={news[0].title} description={news[0].description}></NewsItem>
+                {
+                    news.map((item) => {
+                        return <NewsItem key={item.id} title={item.title} description={item.description}></NewsItem>
+                    })
+                }
             </div>
         );
     }
