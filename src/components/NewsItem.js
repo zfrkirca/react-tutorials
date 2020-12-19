9625e71e8bfca2a4d37cf5a079b5d4c40d0815f1
zafer k <zaferkirca.tr@gmail.com>
2020-12-19 18:31:30 +0300

    className was used

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import '../css/NewsItem.css';
 
 class NewsItem extends Component {
     render() {
         return (
             <div>
+                <h1 className="title">Hello React!</h1>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit
             </div>
         )
