f30e247c6c6ed705987281d8332f0f686d2f5e57
zafer k <zaferkirca.tr@gmail.com>
2020-12-19 21:33:02 +0300

    prop types

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import './App.css';
 import News from "./components/News";
 
 function App() {
+    const news = [
+        {
+            id: 1,
+            title: "title1",
+            description: "description1"
+        }, {
+            id: 2,
+            title: "title2",
+            description: "description2"
+        }, {
+            id: 3,
+            title: "title3",
+            description: "description3"
+        }
+    ];
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
 
-                <News></News>
+                <News news={news} age={30}></News>
             </header>
         </div>
     );
--- src/components/News.js
+++ src/components/News.js
@@ -1,28 +1,27 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import NewsItem from "./NewsItem";
 
 class News extends Component {
-    render() {
-        const news = [
-            {
-                id: 1,
-                title: "title1",
-                description: "description1"
-            }, {
-                id: 2,
-                title: "title2",
-                description: "description2"
-            }, {
-                id: 3,
-                title: "title3",
-                description: "description3"
-            }
-        ];
+    static propTypes = {
+        news: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.number.isRequired,
+                title: PropTypes.string,
+                description: PropTypes.string
+            })
+        ),
+        age: PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.number
+        ]).isRequired
+    }
 
+    render() {
         return (
             <div>
                 {
-                    news.map((item) => {
+                    this.props.news.map((item) => {
                         return <NewsItem key={item.id} title={item.title} description={item.description}></NewsItem>
                     })
                 }
