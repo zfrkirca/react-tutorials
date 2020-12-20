e3a65e774df1e8f80f6f3e4f3f27affcd3737342
zafer k <zaferkirca.tr@gmail.com>
2020-12-20 11:33:41 +0300

    state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import News from "./components/News";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
+class App extends Component {
+    state = {
+        name: 'zafer'
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.changeName = this.changeName.bind(this);
+    }
+
+    changeName() {
+        console.log('button clicked');
+        this.setState({
+            name: "KIRCA"
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <News></News>
-            </header>
-        </div>
-    );
+                <h1>
+                    {this.state.name}
+                </h1>
+
+                <button onClick={this.changeName}>Change the name</button>
+            </div>
+        );
+    }
 }
 
 export default App;
