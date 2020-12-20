3cb531be5204bc1406c182bc0a7dcc64fd83f854
zafer k <zaferkirca.tr@gmail.com>
2020-12-20 21:43:15 +0300

    routing with component

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,29 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import '../css/App.css';
 
+const homepage = () => {
+    return (<h1>Homepage</h1>);
+}
+
+const contact = () => {
+    return (<h1>Contact page</h1>);
+}
+
+const news = () => {
+    return (<h1>News page</h1>);
+}
+
 class App extends Component {
     render() {
         return (
             <Router>
                 <div>
                     {/*exact and strict keywords for full match. search on google*/}
-                    <Route path="/" exact render={() => {
-                        return (<h1>Homepage</h1>);
-                    }}></Route>
+                    <Route path="/" exact strict component={homepage}></Route>
 
-                    <Route path="/contact" exact strict render={() => {
-                        return (<h1>Contact page</h1>);
-                    }}></Route>
+                    <Route path="/contact" exact strict component={contact}></Route>
+
+                    <Route path="/newst" exact strict component={news}></Route>
                 </div>
             </Router>
         );
