73985317a0f6efae641ee0fb38c299603a410106
zafer k <zaferkirca.tr@gmail.com>
2020-12-20 21:24:18 +0300

    fetch function

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,47 @@
-import logo from '../logo.svg';
+import React, {Component} from 'react';
 import '../css/App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-            </header>
-        </div>
-    );
+class App extends Component {
+
+    state = {
+        users: [],
+        isLoading: true
+    };
+
+    componentDidMount() {
+        // it was put because of loading message
+        setTimeout(()=>{
+            fetch('https://jsonplaceholder.typicode.com/users')
+                .then(data => data.json())
+                .then(users => {
+                    console.log(users);
+                    this.setState({
+                        users,
+                        isLoading: false
+                    });
+                })
+        }, 3000);
+    }
+
+    render() {
+        const {isLoading} = this.state;
+
+        return (
+            <div className="App">
+                <h1>Users</h1>
+                {
+                    isLoading ? 'Loading...' : ''
+                }
+                {
+                    this.state.users.map(user =>
+                        <div key={user.id}>
+                            {user.name} - @{user.username}
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
