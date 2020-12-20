b779caed93c3277fb3d29204873236347482795a
zafer k <zaferkirca.tr@gmail.com>
2020-12-20 21:39:46 +0300

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import logo from '../logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import '../css/App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-            </header>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    {/*exact and strict keywords for full match. search on google*/}
+                    <Route path="/" exact render={() => {
+                        return (<h1>Homepage</h1>);
+                    }}></Route>
+
+                    <Route path="/contact" exact strict render={() => {
+                        return (<h1>Contact page</h1>);
+                    }}></Route>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
