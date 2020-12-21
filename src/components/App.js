e58b871da52823448e392996a781b659478b9e8b
zafer k <zaferkirca.tr@gmail.com>
2020-12-21 21:27:37 +0300

    routing redirect

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link, NavLink} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, NavLink, Redirect} from 'react-router-dom';
 import '../css/App.css';
 
 const Homepage = () => {
@@ -14,7 +14,21 @@ const News = (route) => {
     return (<h1>News page: {route.match.params.id}</h1>);
 }
 
+const Profile = () => {
+    return (<h1>Profile page</h1>);
+}
+
 class App extends Component {
+    state = {
+        loggedIn: false
+    }
+
+    onClickButton = () => {
+        this.setState((prevState) => ({
+            loggedIn: !prevState.loggedIn
+        }))
+    }
+
     render() {
         return (
             <Router>
@@ -28,7 +42,13 @@ class App extends Component {
                     {/* You can also use activeClassName */}
                     <NavLink activeStyle={{color: 'red'}} to="/" exact>Homepage</NavLink><br/>
                     <NavLink activeStyle={{color: 'red'}} to="/contact" exact>Contact</NavLink><br/>
-                    <NavLink activeStyle={{color: 'red'}} to="/news/2" exact>News</NavLink>
+                    <NavLink activeStyle={{color: 'red'}} to="/news/2" exact>News</NavLink><br/>
+                    <NavLink activeStyle={{color: 'red'}} to="/profile" exact>Profile</NavLink>
+
+                    <br/><br/>
+
+                    <input type="button" onClick={this.onClickButton}
+                           value={this.state.loggedIn ? 'Log out' : 'Log in'}/>
 
                     {/*exact and strict keywords for full match. search on google*/}
                     <Route path="/" exact strict component={Homepage}></Route>
@@ -36,6 +56,10 @@ class App extends Component {
                     <Route path="/contact" exact strict component={Contact}></Route>
 
                     <Route path="/news/:id" exact strict component={News}></Route>
+
+                    <Route path="/profile" exact strict render={() => (
+                        this.state.loggedIn ? (<Profile/>) : (<Redirect to="/"/>)
+                    )}></Route>
                 </div>
             </Router>
         );
